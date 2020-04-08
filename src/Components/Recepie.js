9e7253009fc57590e759f85b2b9aa5d16856dd90
Diego Taveras <diegotaveras2@outlook.com>
2020-04-08 15:54:56 -0400

    Add info to models

--- src/Components/Recepie.js
+++ src/Components/Recepie.js
@@ -1,9 +1,59 @@
-import React,{useContext} from 'react'
+import React,{useContext,useState} from 'react'
 import { ModalContext } from '../context/ModalContext';
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+
+function getModalStyle() {
+    const top = 50 ;
+    const left = 50;
+  
+    return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+    };
+}
+
+const useStyles = makeStyles(theme => ({
+    paper: {
+      position: 'absolute',
+      width: 450,
+      backgroundColor: theme.palette.background.paper,
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+}));
 
 const Recepie = ({recepie}) => {
 
-    const {setIdRecepie} = useContext(ModalContext);
+    // Modal Configuration
+    const [modalStyle] = useState(getModalStyle);
+    const [open, setOpen] = useState(false);
+
+    const classes = useStyles();
+    const handelOpen = () => {
+        setOpen(true)
+    }
+    const handelClose = () =>{
+        setOpen(false)
+    }
+
+    const {info,setIdRecepie,setRecepie} = useContext(ModalContext);
+
+    const showIngridients = (info) => {
+        let ingridient = [];
+        for(let i = 1; i<16; i++){
+           if(info[`strIngredient${i}`]){
+               ingridient.push(
+                 <li>{info[`strIngredient${i}`]} {info[`strMeasure${i}`]}</li>
+               )
+           } 
+        }
+
+        return ingridient;
+    }
 
     return (
         <div className="col-md-4 mb-3">
@@ -16,10 +66,32 @@ const Recepie = ({recepie}) => {
                         className="btn btn-block btn-primary"
                         onClick={()=>{
                             setIdRecepie(recepie.idDrink)
+                            handelOpen();
                         }}
                     >
                         View Recepie
                     </button>
+                    <Modal
+                        open={open}
+                        onClose={()=>{
+                            setIdRecepie(null)
+                            setRecepie({})
+                            handelClose();
+                        }}
+                    >
+                        <div style={modalStyle} className={classes.paper}>
+                            <h2>{info.strDrink}</h2>
+                            <h3 className="mt-4">Instructions</h3>
+                            <p>
+                                {info.strInstructions}
+                            </p>
+                            <img className="img-fluid my-4" src={info.strDrinkThumb} alt=""/>
+                            <h3>Ingredients and Quanteties</h3>
+                            <ul>
+                                {showIngridients(info)}
+                            </ul>
+                        </div>
+                    </Modal>
                 </div>
             </div>
         </div>
--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -1,15 +1,31 @@
 import React,{createContext,useEffect,useState} from 'react'
+import axios from 'axios'
 
 export const ModalContext = createContext();
 
 const ModalProvider = (props) => {
 
     const [idRecepie, setIdRecepie] = useState(null);
+    const [info,setRecepie] = useState([])
+
+    useEffect(()=>{
+        const getRecepie = async () => {
+            if(!idRecepie) return;
+
+            const url=`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idRecepie}`
+            const result = await axios.get(url)
+            setRecepie(result.data.drinks[0])
+        }
+        getRecepie()
+    },[idRecepie])
 
     return (
         <ModalContext.Provider
             value={{
-                setIdRecepie
+                setIdRecepie,
+                info,
+                setRecepie
+            
             }}
         >
             {props.children}
